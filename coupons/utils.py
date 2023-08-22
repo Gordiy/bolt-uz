eee30a4f397d5bcc859c4d904d573070534902f2
Hordii-Yurii Rushynets <hordii-yurii.rushynets@pe.lifecell.com.ua>
2023-08-22 12:08:43 +0300

    ADD - method that convert InMemoryUploadedFile to TemporaryUploadedFile.

--- coupons/utils.py
+++ coupons/utils.py
@@ -1,4 +1,6 @@
 """Utils for coupons app."""
+from django.core.files.uploadedfile import TemporaryUploadedFile, InMemoryUploadedFile
+
 
 def is_integer(s: str) -> bool:
     """Check if string is integer."""
@@ -15,3 +17,19 @@ def is_float(s: str) -> bool:
         return True
     except ValueError:
         return False
+
+
+def convert_to_temporary_uploaded_file(inmemory_uploaded_file: InMemoryUploadedFile) -> TemporaryUploadedFile:
+    """
+    Convert InMemoryUploadedFile to TemporaryUploadedFile.
+    
+    :param inmemory_uploaded_file: InMemoryUploadedFile.
+    :return TemporaryUploadedFile:
+    """
+    temp_uploaded_file = TemporaryUploadedFile(
+        inmemory_uploaded_file.name, inmemory_uploaded_file.content_type, inmemory_uploaded_file.size,
+        inmemory_uploaded_file.charset, inmemory_uploaded_file.content_type_extra
+    )
+    temp_uploaded_file.file = inmemory_uploaded_file.file
+    
+    return temp_uploaded_file
