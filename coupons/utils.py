9eb72d6a678244d557426308324b1641dffaef90
Hordii-Yurii Rushynets <hordii-yurii.rushynets@pe.lifecell.com.ua>
2023-08-22 13:41:21 +0300

    FIX-tabular

--- coupons/utils.py
+++ coupons/utils.py
@@ -23,22 +23,22 @@ def is_float(s: str) -> bool:
         return False
     
 def deepcopy_temporary_uploaded_file(original_uploaded_file: TemporaryUploadedFile) -> TemporaryUploadedFile:
-        """Make a copy of temporary uploaded file."""
-        temp_file = NamedTemporaryFile(delete=False)
-        temp_file.write(original_uploaded_file.read())
-        temp_file.close()
+    """Make a copy of temporary uploaded file."""
+    temp_file = NamedTemporaryFile(delete=False)
+    temp_file.write(original_uploaded_file.read())
+    temp_file.close()
 
-        copied_uploaded_file = TemporaryUploadedFile(
-            name=original_uploaded_file.name,
-            content_type=original_uploaded_file.content_type,
-            size=original_uploaded_file.size,
-            charset=original_uploaded_file.charset,
-            content_type_extra=original_uploaded_file.content_type_extra,
-        )
-        
-        copied_uploaded_file.file = temp_file
+    copied_uploaded_file = TemporaryUploadedFile(
+        name=original_uploaded_file.name,
+        content_type=original_uploaded_file.content_type,
+        size=original_uploaded_file.size,
+        charset=original_uploaded_file.charset,
+        content_type_extra=original_uploaded_file.content_type_extra,
+    )
+    
+    copied_uploaded_file.file = temp_file
 
-        return copied_uploaded_file
+    return copied_uploaded_file
 
 def convert_to_temporary_uploaded_file(inmemory_uploaded_file: InMemoryUploadedFile) -> TemporaryUploadedFile:
     """
