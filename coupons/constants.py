47f59a9a00760095927cd96a43bb817602d08daa
Hordii-Yurii Rushynets <hordii-yurii.rushynets@pe.lifecell.com.ua>
2023-08-26 16:03:20 +0300

    change PRICE_AND_DISTANCE

--- coupons/constants.py
+++ coupons/constants.py
@@ -1,7 +1,7 @@
 """Constants for coupons app."""
 PRICE_INDEX = 0
 DISTANCE_INDEX = 1
-PRICE_AND_DISTANCE = ((50, 1000), (100, 2000), (150, 3000), (200, 4000))
+PRICE_AND_DISTANCE = ((50, 500), (100, 1000), (150, 1500), (200, 2000))
 
 DEPARTURE = 'відправлення'
 APPOINTMENT = 'призначення'
