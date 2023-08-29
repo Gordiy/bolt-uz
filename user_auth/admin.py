95f2fc6edbc69366413a1945a547286e1aaa58e2
Hordii-Yurii Rushynets <hordii-yurii.rushynets@pe.lifecell.com.ua>
2023-08-29 20:08:33 +0300

    Register admin

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import BoltUser
 
+class BoltUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'distance')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
 
-admin.site.register(BoltUser)
+admin.site.register(BoltUser, BoltUserAdmin)
